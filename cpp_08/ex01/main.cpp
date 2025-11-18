ddd00afe6d28c2ff9924311038ee49d383e88ef6
Tarik Pinarli <tpinarli@c2r3p3.hive.fi>
2025-11-18 16:33:13 +0200

    all

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -2,15 +2,32 @@
 
 int main()
 {
-    Span sp = Span(5);
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    try{
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+        std::cout << sp << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << "Exception cought: " << e.what() << std::endl;
+    }
 
-    std::cout << sp << std::endl;
+    try{
+        Span demo = Span(3);
+        demo.addNumber(6);
+        demo.addNumber(3);
+        demo.addNumber(17);
+        std::cout << demo.shortestSpan() << std::endl;
+        std::cout << demo.longestSpan() << std::endl;
+        std::cout << demo << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cout << "Exception cought: " << e.what() << std::endl;
+    }
     return 0;
 }
